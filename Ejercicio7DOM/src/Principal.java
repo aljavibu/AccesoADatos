765f09fa511648f93897c54eb23222210267ccbe
DAM-2 <DAM-2@PC16>
2020-11-03 13:53:19 +0100

    Ejercicio 7 DOM

---
+++ Ejercicio7DOM/src/Principal.java
@@ -0,0 +1,106 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class Principal {
+
+	private static final String FICHERO_XML = "universidad.xml";
+
+	public static void main(String[] args) {
+
+		try {
+
+			// TODO Auto-generated method stub
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document arbol = builder.parse(new File(FICHERO_XML));
+
+			modificandoArbolDom(arbol);
+
+			// Pasamos el arbol al numero fichero xml
+			Source source = new DOMSource(arbol);
+			Result result = new StreamResult("universidadNuevo.xml");
+			Transformer transformer = TransformerFactory.newInstance().newTransformer();
+			transformer.transform(source, result);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	
+
+	private static void modificandoArbolDom(Document arbol) {
+		Element departamento,empleado;
+		Element raiz = (Element) arbol.getFirstChild();
+		Node puesto;
+		//Lista Departamentos
+		NodeList listaDepartamentos = raiz.getElementsByTagName("departamento");
+		
+		//De la lista de departamentos obtenemos una lista de empleados
+		for (int i = 0; i < listaDepartamentos.getLength(); i++) {
+			departamento = (Element) listaDepartamentos.item(i);
+			NodeList listaEmpleados=departamento.getElementsByTagName("empleado");
+			
+			
+			//Y por cada empleado obtenemos su nodo puesto
+			for(int i1 = 0;i1< listaEmpleados.getLength();i1++) {
+				empleado=(Element)listaEmpleados.item(i1);
+				try {
+					
+					//Obtenemos el nodo puesto del empleado
+					puesto= getNodo("puesto", empleado);
+					//Pasamos el nodo nombre,el empleado y arbol para construir el nuevo elemento apellido
+					
+					if(puesto.getTextContent().equals("Asociado")) {
+						int salario;
+						//Obtenemos el salario
+						salario=Integer.parseInt(empleado.getAttribute("salario"));
+						
+						empleado.setAttribute("salario",String.valueOf(salario*1.05));
+						
+						
+					}
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+			
+		}
+
+	}
+
+	
+
+	private static Node getNodo(String etiqueta, Element ele) throws IOException {
+		
+		//Obtenemos la lista de hijos nombre
+		NodeList listaDeHijosDeEtiqueta = ele.getElementsByTagName(etiqueta);
+
+		if (listaDeHijosDeEtiqueta.getLength() == 0) {
+			throw new IOException("No existe el elemento " + etiqueta);
+
+		} else {
+			//Devolvemos solo el primero 
+			return listaDeHijosDeEtiqueta.item(0);
+		}
+	}
+}
+
